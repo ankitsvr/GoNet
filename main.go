3d8a121c71eebb044f3b364aba25e80645fb509b
ankitsvr <12423302+ankitsvr@users.noreply.github.com>
2025-07-02 01:10:42 +0530

    new begining

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+	"github.com/ankitsvr/GoNet"
 )
-
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usages : Gonet <hostname or IP>")
-		return
-	}
-
-	target := os.Args[1]
-
-	ip := target
-
-	if net.ParseIP(target) = nil {
-		resolvedIP := tools.ResolveDNS(target)
-		if err !=nil {
-			fmt.Prinln("Dns Lookup error")
-			return
-		}
-		fmt.Printf("resolve ip %s", resolvedIP)
-	}
-
-}
