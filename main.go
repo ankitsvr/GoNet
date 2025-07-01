3480bcb8afb8a7998e64cbd294f583a771313b9a
ankitsvr <12423302+ankitsvr@users.noreply.github.com>
2025-07-02 01:19:24 +0530

    testing

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
 import (
-	"github.com/ankitsvr/GoNet"
+	"GoNet/tasks"
+	"fmt"
+	"os"
 )
+
+func main() {
+	if len(os.Args) < 3 || os.Args[1] != "check" {
+		fmt.Println("Usage: go run main.go check <hostname>")
+		return
+	}
+
+	host := os.Args[2]
+	fmt.Println("Running checks for:", host)
+
+	dnsCh := make(chan string)
+	pingCh := make(chan string)
+	traceCh := make(chan string)
+	mtrCh := make(chan string)
+
+	go tasks.PerformDNSLookup(host, dnsCh)
+	go tasks.RunPing(host, pingCh)
+	go tasks.RunTraceroute(host, traceCh)
+	go tasks.RunMTR(host, mtrCh)
+
+	fmt.Println("\n--- DNS Lookup ---")
+	fmt.Print(<-dnsCh)
+
+	fmt.Println("\n--- Ping ---")
+	fmt.Print(<-pingCh)
+
+	fmt.Println("\n--- Traceroute ---")
+	fmt.Print(<-traceCh)
+
+	fmt.Println("\n--- MTR ---")
+	fmt.Print(<-mtrCh)
+}
