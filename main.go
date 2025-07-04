3ef9daa19d9f6397dd9c4d79ea90393eab0fce08
ankitsvr <12423302+ankitsvr@users.noreply.github.com>
2025-07-04 22:35:05 +0530

    A working Gotool for ping and traceroute

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/go-ping/ping"
 )
 
 //logic to check if the given target is IP or hostname
@@ -24,16 +22,15 @@ func dnsLookup(address string) ([]string, error) {
 // logic for the ping
 func doPing(target string) {
 	fmt.Println("Pinging : ", target)
-	pinger, err := ping.NewPinger(target)
+	var cmd *exec.Cmd
+
+	cmd = exec.Command("ping", "-c", "4", target)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("ping error: ", err)
+		fmt.Println("Ping Failed: ", err)
 		return
 	}
-	pinger.Count = 4
-	pinger.Run()
-	stats := pinger.Statistics()
-	fmt.Printf("Results: %+v\n", stats)
-
+	fmt.Println(string(output))
 }
 
 // logic for the traceroute
